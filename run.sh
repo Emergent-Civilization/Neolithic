e69d31e5238f205391f7599e58ce99eab09be576
Bananababooy <oliverremmler889789@gmail.com>
2025-11-04 20:44:26 -0600

    Add run.sh to manage configuration updates
    This script updates the configuration based on the specified branch and checks for hash changes before downloading the latest commit.
    
    Signed-off-by: Bananababooy <oliverremmler889789@gmail.com>

---
+++ run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+
+# ideas for future, for now i just got hash checks done
+# args - default_config_branch, civ_core_branch, spec_branch
+# for each of them, we can have presets, so rather than default-config-base
+# we can just put in base and it replaces it as it needs
+# but if it is null or some other thing that means nothing, it will use whatever the default is
+# most of this is kinda of done, but also kinda of not
+
+updateConfig=${1:-"dev"}
+
+if [ $updateConfig -eq 1 ]; then
+  updateConfig="main"
+fi
+
+if [ "$updateConfig" != "null" ]; then
+  branchData=$(curl -Ls https://api.github.com/repos/Emergent-Civilization/Neolithic/branches/$updateConfig -H "Authorization: token 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")
+
+  branchExistsMsg=$(echo $branchData | jq -r .message)
+  if [ "$branchExistsMsg" == "Branch not found" ]; then
+    echo "Neolithic branch '$updateConfig' does not exist"
+    echo "not updating config"
+  else
+    echo "Updating Config"
+    if [ -e default-config.hash ]; then
+      currentHash=$(cat default-config.hash)
+    else
+      currentHash=""
+    fi
+    echo "Current Hash : $currentHash"
+    newHash=$(curl -Ls https://api.github.com/repos/Emergent-Civilization/Neolithic/branches/$updateConfig -H "Authorization: token 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" | jq -r .commit.sha)
+    echo "New Hash     : $newHash"
+    if [ "$currentHash" != "$newHash" ]; then
+      echo "Hashes different, downloading most recent commit on branch $updateConfig"
+      rm -rf Emergent-Civilization-default-server-config-$newHash default-config.zip
+      curl -L -H "Authorization: token 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" -o default-config.zip https://api.github.com/repos/Emergent-Civilization/default-server-config/zipball/$updateConfig && \
+      unzip default-config.zip  && \
+      cp -r Emergent-Civilization-default-server-config-$newHash/* . && \
+      cp -r Emergent-Civilization-default-server-config-$newHash/.[^.]* . 2>/dev/null || true && \ 
+      rm -rf Emergent-Civilization-default-server-config-$newHash default-config.zip default-config.hash
+      echo $newHash > default-config.hash
+    else
+      echo "Hashes same, doing nothing"
+    fi
+  fi
+fi
+
+
+java -Xms16G -Xmx16G -XX:+UseZGC -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -Dterminal.jline=false -Dterminal.ansi=true -jar server.jar
